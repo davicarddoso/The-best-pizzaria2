08cb2d69668e0200c63e0120581b8971c1d97c4a
davicarddoso <davicardoso20118@gmail.com>
2026-07-01 23:16:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/The-best-pizzaria2"
+  base: "/The-best-pizzaria2/",
   plugins: [react(), tailwindcss()],
 });
